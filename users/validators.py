130b84b70b0a802a358e882726f37721f2ae167e
Андрей <lvov-aa@bk.ru>
2026-08-18 01:01:54 +0300

    feat(users): add field validators

---
+++ users/validators.py
@@ -0,0 +1,16 @@
+import datetime
+
+from django.core.exceptions import ValidationError
+from django.db.models.fields.files import FieldFile
+
+MAX_IMAGE_SIZE_MB = 5
+MAX_IMAGE_SIZE = MAX_IMAGE_SIZE_MB * 1024 * 1024
+
+def validate_image_size(file: FieldFile) -> None:
+    if file.size > MAX_IMAGE_SIZE:
+        raise ValidationError(f'Размер файла не должен превышать {MAX_IMAGE_SIZE_MB} МБ.')
+
+
+def validate_date_of_birth(date_of_birth: datetime.date | None) -> None:
+    if date_of_birth and date_of_birth > datetime.date.today():
+        raise ValidationError('Укажите корректную дату рождения.')
